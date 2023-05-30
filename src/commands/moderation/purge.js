13f56f0e92126ebcc2df09dec0970d35f265fe08
Krisp1k <vladadoskar@gmail.com>
2023-05-30 19:00:27 +0200

    purge, new replies

---
+++ src/commands/moderation/purge.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Vymaže předchozí zprávy')
+        .addIntegerOption(option =>
+            option.setName('počet')
+                .setDescription('Počet zpráv, které chceš mazat (0-100)')
+                .setRequired(true)
+                // .setMaxValue(100)
+                // .setMinValue(0)
+        ),
+    async run(interaction, client) {
+
+        const amount = interaction.options.getInteger('počet');
+
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+            return await interaction.reply({
+                content: 'Na použití tohoto příkazu potřebuješ oprávnění **ManageMessages**.',
+                ephemeral: true
+            });
+        }
+
+        if (amount <= 0 || amount > 100) {
+            return await interaction.reply({
+                content: 'Zadej číslo od 1 do 100 prosím (a taky si prosím kup kredity)',
+                ephemeral: true
+            });
+        }
+
+        try {
+            const channel = interaction.channel;
+            const messages = await channel.messages.fetch({ 
+                limit: amount 
+            });
+
+            await channel.bulkDelete(messages);
+            await interaction.reply({
+                content: `Smazal jsem posledních ${amount} zpráv.`,
+                ephemeral: true
+            });
+
+        } catch (error) {
+            console.log(`Error při mazání posledních ${amount} zpráv: ${error}`);
+
+            await interaction.reply({
+                content: `Error při mazání posledních ${amount} zpráv: ${error}`,
+                ephemeral: true
+            });
+        }
+    },
+};
--- src/functions/tools/randomPletronReply.js
+++ src/functions/tools/randomPletronReply.js
@@ -3,31 +3,51 @@ module.exports = (client) => {
     client.randomPletronReply = async () => {
 
         const replies = [
-            "Neruš, programuju, jsem programátor.",
-            "Měl jsem sen, že jednoho dne budou lidé vynakládat stejnou energii a peníze na skutečné cíle. Ale jsem rád, že jsem se spletl.",
+            "Nerus, programuju, jsem programator.",
+            "Mel jsem sen, ze jednoho dne budou lide vynakladat stejnou energii a penize na skutecne cile. Ale jsem rad, ze jsem se spletl.",
             "Ahoj",
-            "Koupě kreditů ve hře je opravdu klíčem k šťastnému a naplněnému životu. Kdo potřebuje skutečné zážitky, že?",
-            "Tvoje máma",
-            "Pokoslav smrdí",
-            "Nevotravuj a běž si radši koupit kredity",
+            "Koupe kreditu ve hre je opravdu klicem k stastnemu a naplnenemu zivotu. Kdo potrebuje skutecne zazitky, ze?",
+            "Tvoje mama",
+            "Pokoslav smrdi",
+            "Nevotravuj a bez si radsi koupit kredity",
             "Ty jsi ale trubka",
-            "A Nejsi ty náhodou Enchanter?",
-            "Takovou pohádku znam taky",
-            "A nechceš třeba ban?",
-            "Moje mentální vada je vskutku vděčná za váš příspěvek ke koupi dalšího balíčku virtuálního vzduchu.",
-            "Ano, to je pravda. Místo toho jsem radši vytvořil virtuální svět, kde si můžete koupit imaginární věci. Jsem takový filantrop, že ano?",
-            "Nerušte mě, pracuju na naprosté hovadině které dám přehnané staty a pak vám ji prodám za litr",
-            "Ještě chvíli mě otravuj a dam ti BAN!",
+            "A Nejsi ty nahodou Enchanter?",
+            "Takovou pohadku znam taky",
+            "A nechces treba ban?",
+            "Moje mentalni vada je vskutku vdecna za vas prispevek ke koupi dalsiho balicku virtualniho vzduchu.",
+            "Ano, to je pravda. Misto toho jsem radsi vytvoril virtualni svet, kde si muzete koupit imaginarni veci. Jsem takovy filantrop, ze ano?",
+            "Nerus mě, pracuju na naproste hovadine které dam prehnane staty a pak vam ji prodam za litr",
+            "Jeste chvili me otravuj a dam ti BAN!",
             "Zeptej se Werona",
-            "Ty uličníku",
-            "Teď se mi nechce odpovídat, nemam čas",
-            "Pracuju na Dark Elfovi, neruš",
+            "Ty ulicniku",
+            "Ted se mi nechce odpovidat, nemam cas",
+            "Pracuju na Dark Elfovi, nerus",
             "????",
-            "A nevíš kdy Enchanter natočí další video?",
-            "Ty nezlob mě a dám ti práva na redakční sloupek"
-       ];
-    
-       const randomReply = replies[Math.floor(Math.random() * replies.length)]
-       return randomReply;
+            "A nevis kdy Enchanter natoci dalsi video?",
+            "Ty nezlob me a dam ti prava na redakcni sloupek",
+            "Za tohle te abdiknu!!",
+            "Chyba LPho? Kdepak!",
+            "Vite, co ma spolecneho Pan Hradu s moji manzelkou? Na oboje seru.",
+            "Ty mas asi dost velky koule abys tohle rekl",
+            "Nemuzu mluvit, dcera zakopla o server",
+            "Promin, nemohu ti ted odpovidat, protoze jsem prilis zamestnan tym, jak zlepsit svet pomoci pixelovych hrdinu.",
+            "Mozna jsem mel byt spisovatel, protoze vymyslim uzasne pribehy o virtualnich princich a princeznach. Ale radeji prodavam kredity.",
+            "Vitej, chtel jsem te pozdravit slovem, ktere v dnesni dobe prevysuje vsechny emoji na svete: 'Ahoj'.",
+            "Kdo potrebuje skutecne zazitky ze zivota, kdyz si muze kupovat virtualni zbrane a hrdiny? Pan Hradu je klicem k naplnenemu zivotu, samozrejme.",
+            "Ach, tvoje mama. Ta je vseude, i v online hrach. Vecna legenda. Teda, az na Pana Hradu, tam by se na mapu nevesla.",
+            "Pokoslav smrdi? Ne, to je jenom jeho virtualni oder, ktery ma uzasno silu odradit nepritele.",
+            "Prosim, neotravuj me svymi problemi. Radsi si zajdi do obchodu a kup si spetku kreditu.",
+            "Jsi opravdu k nicemu, ale nemusis byt smutny, svet Pana Hradu je tu pro vsechny. Tedy, pokud mas penize, jinak muzes jit vis kam.",
+            "Mam rad pohadky. Ale vikas, co mam jeste rad? Kreditove balicky, ktere si muze kupit v online svete Pana Hradu.",
+            "Ban? To je takove prekvapeni, ktere obcas zpestri den. Mozna bych mel zacit sbirat bany jako sbiratelske karty.",
+            "Ach ty, jsi takovy mistr vyroby trubek.",
+            "Ver mi, zivot je mnohem plnejsi a stastnejsi, kdyz utraci penize za virtualni kredity misto za skutecne zazitky. Nebo ne?",
+            "Jsem tak vytizeny programovanim, ze bych se mohl stat robotem misto clovekem.",
+            "Tak co, stale chces ban? Mozna bych mel tesit lidi jako ty.",
+            "Ty maly darebaku."
+        ];
+
+        const randomReply = replies[Math.floor(Math.random() * replies.length)]
+        return randomReply;
     }
 }
