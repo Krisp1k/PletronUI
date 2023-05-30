0a73140dac9281e6cf1a0e2ea89453b307d6df7b
Krisp1k <vladadoskar@gmail.com>
2023-05-30 14:34:49 +0200

    pocitadlo artu

---
+++ src/commands/basic/pocitadlo.js
@@ -0,0 +1,87 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pocitadlo')
+        .setDescription('Spočítá čas, kdy se rozpadne artefakt podle aktuálních procent')
+        .addNumberOption(option =>
+            option.setName('procenta')
+                .setDescription('Aktuální procenta artefaktu')
+                .setRequired(true)
+        ),
+    async run(interaction, client) {
+
+        const vypocetCasu = (currentPercentage) => {
+            const currentTime = new Date();
+            const currentHours = currentTime.getHours();
+            const currentMinutes = currentTime.getMinutes();
+
+            // Výpočet času dosažení 0%
+            const remainingPercentage = currentPercentage;
+            const remainingDamageCycles = Math.floor(remainingPercentage / 1);
+            const remainingHours = remainingDamageCycles * 8;
+
+            let breakdownTime = new Date();
+            breakdownTime.setHours(currentHours);
+            breakdownTime.setMinutes(currentMinutes);
+
+            if (currentHours < 8) 
+            {
+                breakdownTime.setHours(8);
+                breakdownTime.setMinutes(0);
+            } 
+            else if (currentHours < 16) 
+            {
+                breakdownTime.setHours(16);
+                breakdownTime.setMinutes(0);
+            } 
+            else 
+            {
+                breakdownTime.setHours(24);
+                breakdownTime.setMinutes(0);
+                breakdownTime.setDate(breakdownTime.getDate() + 1);
+            }
+
+            let remainingDays = Math.floor(remainingHours / 24);
+            let remainingHoursInDay = remainingHours % 24;
+
+            while (remainingDays > 0) {
+                breakdownTime.setDate(breakdownTime.getDate() + 1);
+                if (breakdownTime.getDay() !== 0 && breakdownTime.getDay() !== 6) {
+                    remainingDays--;
+                }
+            }
+
+            breakdownTime.setHours(breakdownTime.getHours() + remainingHoursInDay);
+
+            let completeBreakdownTime = new Date(breakdownTime);
+
+            completeBreakdownTime.setDate(breakdownTime.getDate() + 1);
+            completeBreakdownTime.setHours(5);
+            completeBreakdownTime.setMinutes(55);
+
+            if (completeBreakdownTime <= breakdownTime) {
+                completeBreakdownTime.setDate(completeBreakdownTime.getDate() + 1);
+            }
+
+            return {
+                breakdownTime,
+                completeBreakdownTime
+            };
+        }
+
+        const currPercentage = interaction.options.getNumber("procenta")
+        const casyRozpadu = vypocetCasu(currPercentage);
+        const options = { hour: 'numeric', minute: 'numeric' };
+
+        // await interaction.reply({
+        //     content: '```Dosáhnutí 0%: \n- ' + casyRozpadu.breakdownTime.toLocaleString() + '\n\nKompletní rozpad: \n- ' + casyRozpadu.breakdownTime.toLocaleString() + '```',
+        //     ephemeral: true
+        // })
+
+        await interaction.reply({
+            content: `Artefakt dosáhne 0% v čase **${casyRozpadu.breakdownTime.toLocaleTimeString(undefined, options)}**.\n*(Kompletně se rozpadne v 5:55 hodin dalšího dne ráno.)*`,
+            ephemeral: true
+        })
+    }
+}
