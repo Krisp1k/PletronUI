7f80db34b1d20b9a97b12a754dc5a49b96aaed00
Krisp1k <75334703+Krisp1k@users.noreply.github.com>
2024-03-31 21:11:58 +0200

    hotfix (#4)

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on("messageCreate", async (msg) => {
     const isTriggerWord = msg.content.toLowerCase() === triggerWord.toLowerCase();
 
     // REPLIES
-    if (isTriggerWord || msg.content.length <= 5) {
+    if (isTriggerWord || (containsTriggerWord && msg.content.length <= 5)) {
         // pokud zprava je pouze "lp", tak nebudeme odpovidat pres GPT api
         await client.randomPletronReply().then((reply) => {
             msg.channel.send(reply);
