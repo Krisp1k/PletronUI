7ee05d2bdea44913f7f1637379376c11b2fee601
vdoskar <czraketa@gmail.com>
2024-03-31 21:35:17 +0200

    andry hotfix

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.GuildMessageReactions,
-    ],
+	],
 });
 
 client.commandArray = [];
@@ -21,71 +21,83 @@ const triggerWord = "LP";
 
 const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
-    const functionFiles = fs
-        .readdirSync(`./src/functions/${folder}`)
-        .filter((file) => file.endsWith(".js"));
+	const functionFiles = fs
+		.readdirSync(`./src/functions/${folder}`)
+		.filter((file) => file.endsWith(".js"));
 
-    for (const file of functionFiles)
-        require(`./src/functions/${folder}/${file}`)(client);
+	for (const file of functionFiles)
+		require(`./src/functions/${folder}/${file}`)(client);
 }
 
 client.on("messageCreate", async (msg) => {
 
-    const msgAuthor = msg.author.id;
-    if (msg.author.bot) return;
+	const msgAuthor = msg.author.id;
+	if (msg.author.bot && msg.author.id != "1158467740672208969") return;
 
-    msg.content = msg.content.trim();
-    const containsTriggerWord = msg.content.toLowerCase().includes(triggerWord.toLowerCase());
-    const isTriggerWord = msg.content.toLowerCase() === triggerWord.toLowerCase();
+	msg.content = msg.content.trim();
+	const containsTriggerWord = msg.content.toLowerCase().includes(triggerWord.toLowerCase());
+	const isTriggerWord = msg.content.toLowerCase() === triggerWord.toLowerCase();
 
-    // REPLIES
-    if (isTriggerWord || (containsTriggerWord && msg.content.length <= 5)) {
-        // pokud zprava je pouze "lp", tak nebudeme odpovidat pres GPT api
-        await client.randomPletronReply().then((reply) => {
-            msg.channel.send(reply);
-        });
+	// custom andry reply
+	if (msg.channel.id == "1223380151203659856" && msg.author.id == "1158467740672208969") {
+		console.log("andry mluvi")
+		try {
+			const gptReply = (await import("./src/config/OpenAI_API.mjs")).gptReply;
+			const response = await gptReply(msg.content, true);
+			msg.reply(response);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-    } else if (containsTriggerWord) {
-        // pokud zprava obsahuje "lp", ale zaroven to neni pouze samotne slovo, tak si hodime kostkou, zda odpovime random reply nebo pres GPT api
-        try {
-            if (Math.random() < 0.25) { // 75% šance na GPT ?
-                await client.randomPletronReply().then((reply) => {
-                    msg.channel.send(reply);
-                });
-            } else {
-                const gptReply = (await import("./src/config/OpenAI_API.mjs")).gptReply;
-                const response = await gptReply(msg.content);
-                msg.reply(response);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    // MOCK
-    // kontrola, aby nemockoval stickery (neumí to a crashne) [Discord API to nema vyresene asi]
-    if (msg.stickers.size <= 0) {
-        try {
-            const mockData =
-                JSON.parse(fs.readFileSync("src/data/mock.json", "utf8")) || {};
+	// REPLIES
+	if (isTriggerWord || (containsTriggerWord && msg.content.length <= 5)) {
+		// pokud zprava je pouze "lp", tak nebudeme odpovidat pres GPT api
+		await client.randomPletronReply().then((reply) => {
+			msg.channel.send(reply);
+		});
 
-            if (mockData["mocked"][msgAuthor]) {
-                if (msg.content == "" || msg.content == null) {
-                    await msg.channel.send(":thinking:");
-                } else {
-                    await msg.channel.send(msg.content);
-                }
+	} else if (containsTriggerWord) {
+		// pokud zprava obsahuje "lp", ale zaroven to neni pouze samotne slovo, tak si hodime kostkou, zda odpovime random reply nebo pres GPT api
+		try {
+			if (Math.random() < 0.25) { // 75% šance na GPT ?
+				await client.randomPletronReply().then((reply) => {
+					msg.channel.send(reply);
+				});
+			} else {
+				const gptReply = (await import("./src/config/OpenAI_API.mjs")).gptReply;
+				const response = await gptReply(msg.content, false);
+				msg.reply(response);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-                if (msg.attachments.size > 0) {
-                    msg.attachments.forEach(async (attachment) => {
+	// MOCK
+	// kontrola, aby nemockoval stickery (neumí to a crashne) [Discord API to nema vyresene asi]
+	if (msg.stickers.size <= 0) {
+		try {
+			const mockData =
+				JSON.parse(fs.readFileSync("src/data/mock.json", "utf8")) || {};
+
+			if (mockData["mocked"][msgAuthor]) {
+				if (msg.content == "" || msg.content == null) {
+					await msg.channel.send(":thinking:");
+				} else {
+					await msg.channel.send(msg.content);
+				}
+
+				if (msg.attachments.size > 0) {
+					msg.attachments.forEach(async (attachment) => {
 						await msg.channel.send(attachment.url);
 					});
-                }
-            }
-        } catch (error) {
-            console.log("Mock failed, error: ", error);
-        }
-    }
+				}
+			}
+		} catch (error) {
+			console.log("Mock failed, error: ", error);
+		}
+	}
 });
 
 client.handleEvents();
--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: 'ready',
-    once: true,
-    presenceDelay: 30, // seconds
-    fetchClansDelay: 90, // minutes
-    async execute(client) {
+	name: 'ready',
+	once: true,
+	presenceDelay: 30, // seconds
+	fetchClansDelay: 90, // minutes
+	async execute(client) {
 		console.log('jedu jak drak a jsem ' + client.user.tag)
 
 		for (const job of Object.values(client.cronJobs)) {
@@ -13,13 +13,5 @@ module.exports = {
 				console.log("[CRON] Job failed to run");
 			}
 		}
-
-        // // aktivita na DC
-        // client.pickPresence()
-        // setInterval(client.pickPresence, this.presenceDelay * 1000)
-
-        // // fetch klanů
-        // client.fetchClans()
-        // setInterval(client.fetchClans, this.fetchClansDelay * 1000 * this.fetchClansDelay)   
-    }
+	}
 }
